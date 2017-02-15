e1ef9fdb3f3b90606830efc5f72ce8525a5bf440
Dmytro Buglov <dmytro_buglov@epam.com>
2017-02-15 23:55:09 +0200

    added readable /status command

--- src/main/java/com/epam/env/father/bot/commands/StatusCommand.java
+++ src/main/java/com/epam/env/father/bot/commands/StatusCommand.java
@@ -2,6 +2,9 @@ package com.epam.env.father.bot.commands;
 
 import static java.lang.System.lineSeparator;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +20,31 @@ import com.epam.env.father.service.EnvironmentService;
 @CommandComponent
 public class StatusCommand extends TexteReplyBotCommand {
 
-    @Autowired
-    private EnvironmentService environmentService;
+	private static final String LINE_DELIMETER = lineSeparator() + "_____________________________________________"
+			+ lineSeparator();
 
-    public StatusCommand() {
-        super("status", "With this command you can get up to date info regarding envs statuses");
-    }
+	@Autowired
+	private EnvironmentService environmentService;
 
-    @Override
-    protected void sendResponse(SendMessageBuilder answer, AbsSender absSender, Client user) throws TelegramApiException {
-        String envStatus = environmentService.getAllAvailable().stream().map(Environment::toString).collect(Collectors.joining(lineSeparator()));
-        answer.withMessage(envStatus);
-        absSender.sendMessage(answer.build());
-    }
+	public StatusCommand() {
+		super("status", "With this command you can get up to date info regarding envs statuses");
+	}
+
+	@Override
+	protected void sendResponse(SendMessageBuilder answer, AbsSender absSender, Client user)
+			throws TelegramApiException {
+		Map<String, List<Environment>> envsByCountry = environmentService.getAllAvailable().stream()
+				.collect(Collectors.groupingBy(Environment::getCountry));
+		String formattedMessage = new String();
+		for (Entry<String, List<Environment>> entry : envsByCountry.entrySet()) {
+			formattedMessage += LINE_DELIMETER + entry.getKey() + LINE_DELIMETER + formatEnvText(entry.getValue());
+		}
+		answer.withMessage(formattedMessage);
+		absSender.sendMessage(answer.build());
+	}
+
+	private String formatEnvText(List<Environment> envs) {
+		return envs.stream().map(Environment::toString).collect(Collectors.joining(lineSeparator()));
+	}
 
 }
